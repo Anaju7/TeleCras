0c1a2d845aa4a2e0c82f06a25022f94285a968e4
Anaju7 <anajulia.anaju7@gmail.com>
2024-12-02 14:24:17 -0300

    consulta cep

--- TeleCras/src/app/app.component.ts
+++ TeleCras/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TeleCras';
+  cep: string = '';
+  endereco: any = null;
+  crasInfo: string | null = null;
+  error: string | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  buscarCep() {
+    this.error = null;
+    this.endereco = null;
+    this.crasInfo = null;
+
+    if (!this.cep) {
+      this.error = 'Por favor, digite um CEP válido.';
+      console.log("Por favor, digite um CEP válido.")
+      return;
+    }
+
+    const apiUrl = `https://viacep.com.br/ws/${this.cep}/json/`;
+
+    this.http.get(apiUrl).subscribe({
+      next: (response: any) => {
+        if (response.erro) {
+          this.error = 'CEP não encontrado.';
+          console.log('CEP não encontrado.')
+        } else {
+          this.endereco = response;
+          this.definirCrasMaisProximo(response);
+          console.log(response)
+        }
+      },
+      error: () => {
+        this.error = 'Erro ao buscar o CEP. Tente novamente.';
+      },
+    });
+  }
+
+  definirCrasMaisProximo(response: any) {
+    const bairro = response.bairro.toLowerCase();
+    const localidade = response.localidade.toLowerCase();
+    console.log(localidade)
+    // Exemplo de lógica para definir o CRAS mais próximo
+    if (['santo amaro', 'boa vista', 'coelhos', 'soledade', 'recife'].includes(bairro)
+         && localidade === 'recife') {
+      this.crasInfo = `
+        <strong>CRAS Santo Amaro</strong><br/>
+        Rua Treze de Maio, nº 76 – Santo Amaro<br/>
+        Fone: 99251-6813 / 3232-1547 / 3232-1548<br/>
+        E-mail: crasboavista01@gmail.com<br/>
+        (Em frente à clínica Ultra-diagnóstico)
+      `;
+    }
+    if (['ilha joana bezerra', 'cabanga', 'são josé', 'ilha do leite', 'paissandu'].includes(bairro)
+       && localidade === 'recife') {
+      this.crasInfo = `
+        <strong>CRAS Joana Bezerra/Coque</strong><br/>
+        Rua Lourenço de Sá, nº 140 - Ilha Joana Bezerra<br/>
+        Fone: 99264-6612 / 3355-8665<br/>
+        E-mail: crasjoanabezerra@gmail.com<br/>
+        (Compaz Dom Hélder Câmara)
+      `;
+    } 
+    else {
+      this.crasInfo = 'Não há CRAS próximo definido para este bairro.';
+    }
+  }
 }
--- TeleCras/src/app/app.module.ts
+++ TeleCras/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -11,8 +13,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
